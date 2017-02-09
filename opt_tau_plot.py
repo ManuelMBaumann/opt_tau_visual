4dbe582660eec1a60f9cf875195c5f859b781414
ManuelMBaumann <manuelmbaumann@gmail.com>
2017-02-09 16:12:02 +0100

    todo: del previous plots, parse form canvas

--- opt_tau_plot.py
+++ opt_tau_plot.py
@@ -20,9 +20,10 @@ def J(e, w, W, tau=False):
     C_im  = e*(tau.real**2+tau.imag**2)/(2.0*tau.imag*(tau.real*e+tau.imag))
     return np.sqrt(r**2/(R**2-C_im**2+2.0*C_im*c1_im))
   
-def calc_circles(om, tau):
+def calc_circles(freq, tau, e):
+    om  = 2.0*pi*freq*(1.0-1j*e)
     eta = om/(om-tau)
-    e   = -om[0].imag/om[0].real
+    #e   = -om[0].imag/om[0].real
     C   = 0.0 + 1j*( (e*abs(tau)**2)/(2.0*tau.imag*(tau.imag+e*tau.real)) )
     R   = sqrt( abs(tau)**2*(e**2+1.0)/(4.0*(tau.imag+e*tau.real)**2) )
     ck  = np.zeros((len(om),), dtype=complex)
@@ -31,39 +32,39 @@ def calc_circles(om, tau):
     r  = abs(tau/(tau-np.conj(tau)))
     return C, R, ck, r
 
-def draw_circles(tau_re, tau_im, om):
-    C, R, ck, r = calc_circles(om, tau_re+1j*tau_im)
+def draw_circles(tau_re, tau_im, eps, freq):
+    om  = 2.0*pi*freq*(1.0-1j*eps)
+    NOP = 1000
+    th  = np.linspace(0.0, 2.0*pi, NOP)
+    
+    C, R, c, r = calc_circles(freq, tau_re+1j*tau_im, eps)
     X   = R*np.cos(th)+C.real
     Y   = R*np.sin(th)+C.imag
-    [line] = ax.plot(X, Y, 'k')
+
+    ax.plot(X, Y, 'k')
     ax.plot(C.real, C.imag, 'kx', markersize=10)
     
     for k in range(0,len(om)):
-        x = r*np.cos(th)+ck[k].real
-        y = r*np.sin(th)+ck[k].imag
+        x = r*np.cos(th)+c[k].real
+        y = r*np.sin(th)+c[k].imag
         ax.plot(x, y, col[k]+'--')
-        ax.plot(ck.real, ck.imag, col[k]+'x', markersize=10)
-            
-    return [line]
+        ax.plot(c[k].real, c[k].imag, color=col[k], marker='x', markersize=10)
+        
+    ax.axhline(linewidth=0.5, color='k')
+    ax.axvline(linewidth=0.5, color='k')
     
-
-def signal(amp, freq):
-    return amp * sin(2 * pi * freq * t)
 
 axis_color = 'lightgoldenrodyellow'
 fig = plt.figure()
 
 # Draw the plot
 Nom  = 5
-fmin = 1
-fmax = 9
-eps  = 0.05
+fmin = 1.0
+fmax = 9.0
+eps  = 0.5
 freq = np.linspace(fmin,fmax,Nom)
 om   = 2.0*np.pi*freq*(1.0-1j*eps)
 tau = opt_tau_anal(eps,om[0].real,om[-1].real) 
-
-NOP = 1000
-th  = np.linspace(0.0,2.0*pi,NOP)
 
 cc  = list('grcmy')
 col = list('b')
@@ -74,43 +75,41 @@ for k in range(1,Nom-1):
         j=0
     col.append(cc[j])
 col.append('b')
-
-eta = om/(om-tau)
     
 ax = fig.add_subplot(111)
 fig.subplots_adjust(left=0.25, bottom=0.25)
-t = np.arange(-5, 5, 0.01)
-amp_0 = tau.real
-freq_0 = tau.imag
-[line] = draw_circles(tau.real, tau.imag, om)
-#[line] = ax.plot(t, signal(amp_0, freq_0), linewidth=2, color='red')
+draw_circles(tau.real, tau.imag, eps, freq)
 ax.set_xlim([-5, 5])
 ax.set_ylim([-5, 5])
 ax.axis('equal')
 
 
 # Add two sliders for tweaking the parameters
-amp_slider_ax  = fig.add_axes([0.25, 0.15, 0.65, 0.03], axisbg=axis_color)
-amp_slider = Slider(amp_slider_ax, r'Re($\tau$)', 0.1, 10.0, valinit=amp_0)
-freq_slider_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03], axisbg=axis_color)
-freq_slider = Slider(freq_slider_ax, r'Im($\tau$)', 0.1, 30.0, valinit=freq_0)
+eps_slider_ax     = fig.add_axes([0.25, 0.05, 0.65, 0.03], axisbg=axis_color)
+eps_slider        = Slider(eps_slider_ax, r'$\epsilon$', 0.0, 1.0, valinit=eps)
+tau_re_slider_ax  = fig.add_axes([0.25, 0.15, 0.65, 0.03], axisbg=axis_color)
+tau_re_slider     = Slider(tau_re_slider_ax, r'Re($\tau$)', 0.0, om[-1].real, valinit=tau.real)
+tau_im_slider_ax  = fig.add_axes([0.25, 0.1, 0.65, 0.03], axisbg=axis_color)
+tau_im_slider     = Slider(tau_im_slider_ax, r'Im($\tau$)', -om[-1].real, 0.0, valinit=tau.imag)
 def sliders_on_changed(val):
-    line.set_ydata(signal(amp_slider.val, freq_slider.val))
+    draw_circles(tau_re_slider.val, tau_im_slider.val, eps_slider.val, freq)
     fig.canvas.draw_idle()
-amp_slider.on_changed(sliders_on_changed)
-freq_slider.on_changed(sliders_on_changed)
+eps_slider.on_changed(sliders_on_changed)
+tau_re_slider.on_changed(sliders_on_changed)
+tau_im_slider.on_changed(sliders_on_changed)
 
 # Add a button for resetting the parameters
-reset_button_ax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+reset_button_ax = fig.add_axes([0.8, 0.25, 0.1, 0.04])
 reset_button = Button(reset_button_ax, r"Go to $\mathbf{\tau^\ast}$", color=axis_color, hovercolor='0.975')
 def reset_button_on_clicked(mouse_event):
-    freq_slider.reset()
-    amp_slider.reset()
+    eps_slider.reset()
+    tau_re_slider.reset()
+    tau_im_slider.reset()
 reset_button.on_clicked(reset_button_on_clicked)
 
 # Add a set of radio buttons for changing color
 color_radios_ax = fig.add_axes([0.025, 0.7, 0.15, 0.2], axisbg=axis_color)
-color_radios = RadioButtons(color_radios_ax, (r'$\epsilon = $', 'fmin = ', 'fmax = ', r'$N_f = $'), active=0)
+color_radios = RadioButtons(color_radios_ax, ('fmin = ', 'fmax = ', r'$N_f = $'), active=0)
 def color_radios_on_clicked(label):
     line.set_color(label)
     fig.canvas.draw_idle()
